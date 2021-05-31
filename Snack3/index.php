39f46b4d2607fa639d8fbf22fe6a0b22df462d34
andreicismas <andrei_cismas@yahoo.it>
2021-05-31 20:06:37 +0200

    end ess snack3

--- Snack3/index.php
+++ Snack3/index.php
@@ -43,3 +43,38 @@ $posts = [
     ],
 ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack_3  </title>
+</head>
+<body style="text-align:center  ">
+        <h1>Posts list</h1>
+
+        <?php
+        // ciclo tutti i post
+        foreach ($posts as $date => $value) { ?>
+            <h2 style="color : red"><?php echo $date ?></h2>
+                    <?php
+                        // recupero dati date
+                        foreach ($value as $post) { 
+                    ?>
+                    
+                    <div >
+                        <h4 style="text-decoration : underline" ><?php echo $post['title']?></h4>
+                        <p><?php echo $post['text'] ?></p>
+                        <h6 style="color:blue"><?php echo $post['author'] ?></h6>
+                    </div>
+
+                    <?php
+                } 
+                ?>
+            <?php
+        } 
+        ?>
+</body>
+</html>
